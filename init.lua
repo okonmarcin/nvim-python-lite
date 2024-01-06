189044a032bdb4a174eded29074f07e17162b704
okonmarcin <marcin.okon@flyrlabs.com>
2024-01-06 08:15:40 +0100

    Update configuration

--- init.lua
+++ init.lua
@@ -4,34 +4,36 @@ require("plugins_config.lualine_conf")
 require("plugins_config.lsp_conf")
 require("plugins_config.harpoon_conf")
 require("plugins_config.indent_conf")
+require("plugins_config.noice_conf")
+telescope_builtins = require("telescope.builtin")
 
+local map = vim.keymap.set
 
 vim.cmd.colorscheme "catppuccin"
-vim.o.background = 'dark'
 vim.o.splitbelow = true
+vim.o.relativenumber = true
 vim.wo.number = true
 vim.o.guicursor = 'i:block'
-vim.g["deoplete#enable_at_startup"] = 1
-
--- Change leader key
 vim.g.mapleader = ' '
 
-local map = vim.api.nvim_set_keymap
-vim.keymap.set('n', ';', ':')
 map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', { expr = true, noremap = true })
 map('t', '<Esc>', '<C-\\><C-n>', {})
+map("n", "J", "mzJ`z")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+map("n", "<space><space>", ':source<CR>', {})
+map('n', '<leader>ff', telescope_builtins.find_files, {})
+map('n', '<leader>fg', telescope_builtins.live_grep, {})
+map('n', '<leader>fb', telescope_builtins.buffers, {})
+map('n', '<leader>fh', telescope_builtins.help_tags, {})
+map('n', '<leader>fr', telescope_builtins.lsp_references, {})
+map('n', '<leader>fd', telescope_builtins.lsp_definitions, {})
+map('n', '<leader>fgb', telescope_builtins.git_branches, {})
 
-telescope_builtins = require("telescope.builtin")
-vim.keymap.set('n', '<leader>ff', telescope_builtins.find_files, {})
-vim.keymap.set('n', '<leader>fg', telescope_builtins.live_grep, {})
-vim.keymap.set('n', '<leader>fb', telescope_builtins.buffers, {})
-vim.keymap.set('n', '<leader>fh', telescope_builtins.help_tags, {})
-vim.keymap.set('n', '<leader>fr', telescope_builtins.lsp_references, {})
-vim.keymap.set('n', '<leader>fd', telescope_builtins.lsp_definitions, {})
-vim.keymap.set('n', '<leader>fgb', telescope_builtins.git_branches, {})
--- Disable arrows in nmode
 for _, mode in pairs({ 'n', 'v', 'x' }) do
 	for _, key in pairs({ '<Up>', '<Down>', '<Left>', '<Right>' }) do
-		vim.keymap.set(mode, key, '<nop>')
+		map(mode, key, '<nop>')
 	end
 end
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,5 @@
 local fn = vim.fn
 
--- Automatically install packer
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
 	PACKER_BOOTSTRAP = fn.system({
@@ -15,7 +14,6 @@ if fn.empty(fn.glob(install_path)) > 0 then
 	vim.cmd([[packadd packer.nvim]])
 end
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd([[
   augroup packer_user_config
     autocmd!
@@ -23,13 +21,11 @@ vim.cmd([[
   augroup end
 ]])
 
--- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
 	return
 end
 
--- Have packer use a popup window
 packer.init({
 	display = {
 		open_fn = function()
@@ -38,7 +34,6 @@ packer.init({
 	},
 })
 
--- Install your plugins here
 return packer.startup(function(use)
 	use("wbthomason/packer.nvim") -- Have packer manage itself	
 	use('jiangmiao/auto-pairs')
@@ -85,6 +80,10 @@ return packer.startup(function(use)
 		requires = { { "nvim-lua/plenary.nvim" } }
 	}
 	use('lukas-reineke/indent-blankline.nvim')
+	use('folke/noice.nvim')
+	use('MunifTanjim/nui.nvim')
+	use('rcarriga/nvim-notify')
+	use('terryma/vim-multiple-cursors')
 	if PACKER_BOOTSTRAP then
 		require("packer").sync()
 	end
