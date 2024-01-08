9732e89e71382946af509921dae7834ddc3e98bb
okonmarcin <marcin.okon@flyrlabs.com>
2024-01-08 07:29:30 +0100

    Cleanup some winblend settings

--- lua/plugins_config/harpoon_conf.lua
+++ lua/plugins_config/harpoon_conf.lua
@@ -6,10 +6,3 @@ vim.keymap.set("n", "hs", function() harpoon.ui:toggle_quick_menu(harpoon:list()
 vim.keymap.set("n", "hm", function() harpoon:list():append() end)
 vim.keymap.set("n", "hp", function() harpoon:list():prev() end)
 vim.keymap.set("n", "hn", function() harpoon:list():next() end)
-
-vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = "harpoon",
-	callback = function()
-		vim.opt.winblend = 50
-	end,
-})
